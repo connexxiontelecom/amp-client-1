dea0ad519f01a3ec5f6eb450054ee069fb525b8b
cjamanambu <oamanambu@yahoo.com>
2021-04-19 14:48:00 +0100

    'Implemented sweeping changes'

--- src/api-config.js
+++ src/api-config.js
@@ -1,6 +1,6 @@
 export default {
-  endpoint: 'http://localhost:8080',
-  // endpoint: 'https://amp-api.connexxiontelecom.com/public',
-  frontend: 'http://localhost:8081',
-  // frontend: 'https://amp.connexxiontelecom.com',
+  // endpoint: 'http://localhost:8080',
+  endpoint: 'https://amp-api.connexxiontelecom.com/public',
+  // frontend: 'http://localhost:8081',
+  frontend: 'https://amp.connexxiontelecom.com',
 }
