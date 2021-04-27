d40089d46f0cb34be26d64e1a827d82793f41a08
cjamanambu <oamanambu@yahoo.com>
2021-04-27 09:18:33 +0100

    Removed the forgot password link

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -79,9 +79,9 @@
               <b-form-group>
                 <div class="d-flex justify-content-between">
                   <label for="login-password">Password</label>
-                  <b-link :to="{name:'auth-forgot-password-v2'}">
-                    <small>Forgot Password?</small>
-                  </b-link>
+<!--                  <b-link :to="{name:'auth-forgot-password-v2'}">-->
+<!--                    <small>Forgot Password?</small>-->
+<!--                  </b-link>-->
                 </div>
                 <validation-provider
                   #default="{ errors }"
