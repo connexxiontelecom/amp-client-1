f6c68d2afbe231d9ca98fd9d79f6418fdf9902b3
cjamanambu <oamanambu@yahoo.com>
2021-05-18 11:42:59 +0100

    switches domain endpoint for the application

--- src/api-config.js
+++ src/api-config.js
@@ -2,5 +2,5 @@ export default {
   endpoint: 'http://localhost:8080',
   // endpoint: 'https://amp-api.connexxiontelecom.com/public',
   frontend: 'http://localhost:8081',
-  // frontend: 'https://amp.connexxiontelecom.com',
+  // frontend: 'https://app-amp.connexxiontelecom.com',
 }
